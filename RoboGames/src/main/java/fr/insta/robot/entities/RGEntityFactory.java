ea11e061da249b258938a7c878fe7cb632514c31
Lam <tanglam77@gmail.com>
2015-11-06 11:26:14 +0100

    first commit

--- RoboGames/src/main/java/fr/insta/robot/entities/RGEntityFactory.java
+++ RoboGames/src/main/java/fr/insta/robot/entities/RGEntityFactory.java
@@ -1,10 +1,5 @@
 package fr.insta.robot.entities;
 
-import fr.insta.robot.bo.EvenementEntity;
-import fr.insta.robot.bo.HabilitationEntity;
-import fr.insta.robot.bo.InformationsEntity;
-import fr.insta.robot.bo.LiveEntity;
-import fr.insta.robot.bo.RoleEntity;
 import fr.insta.robot.bo.UserEntity;
 
 /**
@@ -17,46 +12,6 @@ public final class RGEntityFactory {
      */
     private RGEntityFactory() {
         super();
-    }
-    
-    /**
-     * Méthode qui crée et renvoie un objet de type EvenementEntity.
-     * @return un EvenementEntity
-     */
-    public static EvenementEntity getEvenementEntityInstance() {
-    	return new EvenementEntityImpl();
-    }
-    
-    /**
-     * Méthode qui crée et renvoie un objet de type HabilitationEntity.
-     * @return un HabilitationEntity
-     */
-    public static HabilitationEntity getHabilitationEntityInstance() {
-    	return new HabilitationEntityImpl();
-    }
-    
-    /**
-     * Méthode qui crée et renvoie un objet de type InformationsEntity.
-     * @return un InformationsEntity
-     */
-    public static InformationsEntity getInformationEntityInstance() {
-    	return new InformationsEntityImpl();
-    }
-    
-    /**
-     * Méthode qui crée et renvoie un objet de type LiveEntity.
-     * @return un LiveEntity
-     */
-    public static LiveEntity getLiveEntityInstance() {
-    	return new LiveEntityImpl();
-    }
-    
-    /**
-     * Méthode qui crée et renvoie un objet de type RoleEntity.
-     * @return un RoleEntity
-     */
-    public static RoleEntity getRoleEntityInstacen() {
-    	return new RoleEntityImpl();
     }
     
     /**
