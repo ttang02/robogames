2493d8f646c9825affaf49bfe25a6fcf281af8a6
Didjo972 <dim_madikera_972@hotmail.fr>
2015-11-06 11:53:28 +0100

    - correction apporté au projet

--- RoboGames/src/main/java/fr/insta/robot/AbstractServiceFactory.java
+++ RoboGames/src/main/java/fr/insta/robot/AbstractServiceFactory.java
@@ -2,55 +2,54 @@ package fr.insta.robot;
 
 import org.junit.BeforeClass;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
-
-///**
-// * Factory des services.
-// */
-//public class AbstractServiceFactory {
-//	
-//	static ConfigurableApplicationContext context;
-//
-//	/**
-//	 * Initialisation Environnement
-//	 */
-//	@BeforeClass
-//	public static void initEnvironment() {
-//		
-//		System.out.println("load context");
-//		context = new ClassPathXmlApplicationContext("spring/application-config.xml");
-//	}
-//
-//	protected static ConfigurableApplicationContext getContext() {
-//		return context;
-//	}
-//}
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Factory des services.
  */
 public class AbstractServiceFactory {
 	
-	static GenericXmlApplicationContext context;
+	static ConfigurableApplicationContext context;
 
 	/**
 	 * Initialisation Environnement
 	 */
 	@BeforeClass
 	public static void initEnvironment() {
-		// Initial spring context with profiles
-	    context= new GenericXmlApplicationContext();
-	    ConfigurableEnvironment env = (ConfigurableEnvironment) context.getEnvironment();
-	    env.setDefaultProfiles("dev");
-	    context.load("spring/application-config.xml");
-	    context.refresh();
 		
 		System.out.println("load context");
+		context = new ClassPathXmlApplicationContext("spring/application-config.xml");
 	}
 
 	protected static ConfigurableApplicationContext getContext() {
 		return context;
 	}
 }
+
+///**
+// * Factory des services.
+// */
+//public class AbstractServiceFactory {
+//	
+//	static GenericXmlApplicationContext context;
+//
+//	/**
+//	 * Initialisation Environnement
+//	 */
+//	@BeforeClass
+//	public static void initEnvironment() {
+//		// Initial spring context with profiles
+//	    context= new GenericXmlApplicationContext();
+//	    ConfigurableEnvironment env = (ConfigurableEnvironment) context.getEnvironment();
+//	    env.setDefaultProfiles("dev");
+//	    context.load("spring/application-config.xml");
+//	    context.refresh();
+//		
+//		System.out.println("load context");
+//	}
+//
+//	protected static ConfigurableApplicationContext getContext() {
+//		return context;
+//	}
+//}
 
